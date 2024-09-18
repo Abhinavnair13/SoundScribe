22e97ea17a27bd5c1949eb1dbdb80ec0d6ac9109
abhinav skill <abhinav.nair@skilltelligent.com>
2024-09-18 17:30:50 +0530

    Completed whisper worker.
    Implemented a web worker using Whisper.js to enable efficient and privacy-focused client-side audio transcription. This removes the reliance on server-side processing, enhancing performance and user privacy. The application now leverages the browser's capabilities to transcribe audio directly, providing a more streamlined and secure user experience.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,20 @@ import React from "react";
 export default function Header() {
   return (
     <header className="flex items-center justify-between gap-4 p-4 ">
-      <h1 className="font-medium">
-        Sound<span className="text-purple-400 bold">Scribe</span>
-      </h1>
-      <button
+      <a href="/">
+        <h1 className="font-medium">
+          Sound<span className="text-purple-400 bold">Scribe</span>
+        </h1>
+      </a>
+      <a
+        href="/"
         className="flex items-center gap-2
       specialBtn px-4 py-2 rounded-lg text-purple-400
       "
       >
         <p>New</p>
         <i className="fa-solid fa-plus"></i>
-      </button>
+      </a>
     </header>
   );
 }
